30c87a908b905b7187587ce70314b45d06272282
GladysKigen <gladys.kigen@student.moringa.com>
2023-01-24 14:40:17 +0300

    tasklist update

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,20 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({tasks, deleteData}) {
+
+  const tasklist = tasks.map((task) => (
+    <Task
+    key={task.input}
+    input={task.input}
+    category={task.category}
+    deleteData={deleteData}
+    />
+  ))
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      {tasklist}
     </div>
   );
 }
